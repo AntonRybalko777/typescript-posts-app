ef8b24fd4f4950cf25d16f38fb5cc6af4f9d968c
AntonRybalko777 <126842693+AntonRybalko777@users.noreply.github.com>
2024-01-24 13:25:31 +0000

    Deploying to gh-pages from @ AntonRybalko777/typescript-posts-app@a5f2e5587d875e6eb332634a73a7f87c74371da4 🚀

---
+++ static/js/864.07d8eb65.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[864],{9864:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var r=n(184);function c(){return(0,r.jsx)("div",{children:(0,r.jsx)("h2",{style:{textAlign:"center"},children:"Admin page"})})}}}]);
+//# sourceMappingURL=864.07d8eb65.chunk.js.map
